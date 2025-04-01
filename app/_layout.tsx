4f716b56f72f731add2464a60006a86b0585bf02
anthonykijoy <antoni.kijjaroentorn@gmail.com>
2025-04-01 14:03:45 +0200

    feat: add theme, profile screen, and tab navigation
    - Introduce a new theme file with color definitions
    - Add a new profile screen component
    - Replace stack navigation with tab navigation using icons
    - Update dependencies to include @expo/vector-icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,31 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
+import { theme } from "../theme";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorGreen }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "home",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Entypo name="leaf" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "profile",
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
