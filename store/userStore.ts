7a3c390358d0e396defc24dfe9a7e0e395f18c78
anthonykijoy <antoni.kijjaroentorn@gmail.com>
2025-04-03 17:09:58 +0200

    feat(userStore): add AsyncStorage persistence for user store
    Implement persistence for the user store using `@react-native-async-storage/async-storage` to ensure the onboarding state is saved across app restarts. This enhances user experience by maintaining state consistency.

--- store/userStore.ts
+++ store/userStore.ts
@@ -1,15 +1,30 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type UserStore = {
   hasFinishedOnboarding: boolean;
   toggleHadOnboarded: () => void;
 };
 
-export const useUserStore = create<UserStore>((set) => ({
-  hasFinishedOnboarding: false,
-  toggleHadOnboarded: () => {
-    set((state) => {
-      return { ...state, hasFinishedOnboarding: !state.hasFinishedOnboarding };
-    });
-  },
-}));
+export const useUserStore = create(
+  persist<UserStore>(
+    (set) => ({
+      hasFinishedOnboarding: false,
+      toggleHadOnboarded: () => {
+        set((state) => {
+          return {
+            ...state,
+            hasFinishedOnboarding: !state.hasFinishedOnboarding,
+          };
+        });
+      },
+    }),
+    {
+      name: "plantly-user-store",
+      storage: createJSONStorage(() => AsyncStorage),
+      // eslint-disable-next-line
+    }
+    // eslint-disable-next-line
+  )
+);
